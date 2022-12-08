033eaac896450b92ebec802efac27851623823d6
MagnerAntonin <118823120+MagnerAntonin@users.noreply.github.com>
2022-12-08 13:56:23 +0100

    Add files via upload

---
+++ Voitures-Garages-main/src/main/java/garages/Voiture.java
@@ -0,0 +1,109 @@
+package garages;
+
+import java.io.PrintStream;
+import java.util.*;
+
+public class Voiture {
+
+	private final String immatriculation;
+	private final List<Stationnement> myStationnements = new LinkedList<>();
+
+	public Voiture(String i) {
+		if (null == i) {
+			throw new IllegalArgumentException("Une voiture doit avoir une immatriculation");
+		}
+
+		immatriculation = i;
+	}
+
+	public String getImmatriculation() {
+		return immatriculation;
+	}
+
+	/**
+	 * Fait rentrer la voiture au garage
+	 * Précondition : la voiture ne doit pas être déjà dans un garage
+	 *
+	 * @param g le garage où la voiture va stationner
+	 * @throws java.lang.Exception Si déjà dans un garage
+	 */
+	public void entreAuGarage(Garage g) throws Exception {
+		if(this.estDansUnGarage()) {
+			throw new UnsupportedOperationException("La voiture est déjà dans un garage");
+		}
+
+		Stationnement s = new Stationnement(this, g);
+		myStationnements.add(s);
+	}
+
+	/**
+	 * Fait sortir la voiture du garage
+	 * Précondition : la voiture doit être dans un garage
+	 *
+	 * @throws java.lang.Exception si la voiture n'est pas dans un garage
+	 */
+	public void sortDuGarage() throws Exception {
+		if(!this.estDansUnGarage()) {
+			throw new UnsupportedOperationException("La voiture n'est pas dans un garage");
+		}
+
+		this.myStationnements.get(this.myStationnements.size() - 1).terminer();
+
+	}
+
+	/**
+	 * @return l'ensemble des garages visités par cette voiture
+	 */
+	public Set<Garage> garagesVisites() {
+
+		Set <Garage> garages = new HashSet<>();
+		for(Stationnement s : this.myStationnements) {
+			garages.add(s.getGarage());
+		}
+
+		return garages;
+
+	}
+
+	/**
+	 * @return vrai si la voiture est dans un garage, faux sinon
+	 */
+	public boolean estDansUnGarage() {
+
+		if(this.myStationnements.isEmpty()) { return false; }
+		return this.myStationnements.get(this.myStationnements.size() - 1).estEnCours();
+
+	}
+
+	/**
+	 * Pour chaque garage visité, imprime le nom de ce garage suivi de la liste des
+	 * dates d'entrée / sortie dans ce garage
+	 * <br>
+	 * Exemple :
+	 * 
+	 * <pre>
+	 * Garage Castres:
+	 *		Stationnement{ entree=28/01/2019, sortie=28/01/2019 }
+	 *		Stationnement{ entree=28/01/2019, en cours }
+	 *  Garage Albi:
+	 *		Stationnement{ entree=28/01/2019, sortie=28/01/2019 }
+	 * </pre>
+	 *
+	 * @param out l'endroit où imprimer (ex: System.out)
+	 */
+	public void imprimeStationnements(PrintStream out) {
+
+		String impression = "";
+		for(Garage g : this.garagesVisites()) {
+			impression += "\n" + g.toString();
+			for(Stationnement s : this.myStationnements) {
+				if (s.getGarage() == g) {
+					impression += "\n	" + s.toString();
+				}
+			}
+		}
+
+		out.println(impression);
+	}
+
+}
